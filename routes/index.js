caa969e03d61439451ca57f2f07240b1a171bfb1
Suren <surenatoyan93@gmail.com>
2016-09-25 17:07:16 +0400

    Back&Front: Create 404 Page Not found route and html page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 module.exports = function(app) {
 
-  app.get('/',        	require('./loginPage').get);
+  app.get('/',        		require('./loginPage').get);
 
-  app.get('/account', 	require('./accountPage').get);
+  app.get('/account', 		require('./accountPage').get);
 
-  app.get('/account/*', require('./accountPage').get);
+  app.get('/404', 	require('./404').get);
 }
